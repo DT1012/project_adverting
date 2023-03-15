4c753b40ca7200230b0ced2759cf642392411ccf
DT1012 <anhtai10122001@gmail.com>
2023-03-15 18:58:20 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const work_nav = $$('.work-nav-item');
+const work_box = $$('.work-box-list');
+// console.log(work_box, work_nav);
