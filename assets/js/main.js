abab8cf00452dd47a7b70f0316b19c5e9dfcc907
DT1012 <anhtai10122001@gmail.com>
2023-03-16 03:29:31 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,32 @@ const $$ = document.querySelectorAll.bind(document);
 const work_nav = $$('.work-nav-item');
 const work_box = $$('.work-box-list');
 // console.log(work_box, work_nav);
+const navActive = $('.work-nav-item.active');
+const retangle = $('.work-nav .retangle3');
+
+
+requestIdleCallback(function() {
+    retangle.style.left = navActive.offsetLeft + "px";
+    retangle.style.width = navActive.offsetWidth + "px";
+});
+const all = $$('.work-box-list.all');
+const cpm = $$('.work-box-list.cpm');
+const pr = $$('.work-box-list.pr');
+const phygital = $$('.work-box-list.phygital');
+const email = $$('.work-box-list.email');
+const creative = $$('.work-box-list.creative');
+
+
+work_nav.forEach((nav, index) => {
+    const box = work_box[index];
+    nav.onclick = function() {
+
+        $('.work-nav-item.active').classList.remove("active");
+        $('.work-box-list.active').classList.remove("active");
+
+        retangle.style.left = this.offsetLeft + "px";
+        retangle.style.width = this.offsetWidth + "px";
+        this.classList.add('active');
+        box.classList.add('active');
+    }
+});
